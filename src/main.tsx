e50f5cc90db487dfd52dc8b51d67fea626a30d89
Alexei Karikov <karikov.alist.ru@gmail.com>
2025-02-17 13:31:01 +0500

    Integrate Redux for state management and add Toaster component for notifications
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider as ReduxProvider } from 'react-redux';
 import './index.css'
 import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
+import { setupStore } from './store/index.ts';
+import { Toaster } from './components/ui/toaster.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider>
+  <ReduxProvider store={setupStore()}>
+    <Provider forcedTheme='light'>
+      <Toaster />
       <App />
     </Provider>
-  </StrictMode>,
+  </ReduxProvider>,
 )
